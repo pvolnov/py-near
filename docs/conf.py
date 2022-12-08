d552268e2900d03a485cd7c630a4f6155dea68a4
Petr Volnov <neafiol.io@gmail.com>
2022-12-07 23:27:51 -0800

    feat: docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = 'latest'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
