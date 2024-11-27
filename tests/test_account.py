f1a99f0a96c09fc9edc8eadeb17e97ab576695f6
Peter Volnov <neafiol.io@gmail.com>
2024-11-26 19:35:54 -0800

    feat: fix tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,2 +1,13 @@
-async def test_account_get_balance(account):
-    assert await account.get_balance() > 0
+import pytest
+
+from py_near.account import Account
+
+
+@pytest.mark.asyncio
+async def test_account_get_balance():
+    acc = Account(
+        account_id="bob.testnet",
+        rpc_addr="https://rpc.testnet.near.org",
+    )
+
+    assert await acc.get_balance(account_id=acc.account_id) > 0
